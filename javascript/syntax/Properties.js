10a80074bd929d83b25f4f0ba6865f689ee1e5e9
kyungseopk1m <kks0919@kakao.com>
2023-05-01 21:55:03 +0900

    Properties

---
+++ javascript/syntax/Properties.js
@@ -0,0 +1,23 @@
+// 12-1
+// 속성에 접근할 때는 마침표 사용
+const luke = {
+    jedi: true,
+    age: 28,
+};
+
+const isJedi = luke.jedi;
+
+
+// 12-2
+// 변수를 사용해 속성에 접근할 때는 대괄호 [] 사용
+
+function getProp(prop) {
+    return luke[prop];
+}
+
+const isJedi1 = getProp('jedi');
+
+
+// 12-3
+// 제곱 계산을 할 때는 제곱 연산자 **를 사용
+const binary = 2 ** 10;
