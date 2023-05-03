c1dd6d570a9d532e4c46b9d0884c048453e0e427
kyungseopk1m <kks0919@kakao.com>
2023-05-04 02:40:39 +0900

    Comments-2

--- javascript/syntax/Comments.js
+++ javascript/syntax/Comments.js
@@ -23,3 +23,36 @@ const active = true;  // is current tab
 // good
 // is current tab
 const active = true;
+
+
+// 18-3
+// 모든 주석은 공백으로 시작
+
+
+// 18-4
+// 문제를 지적하고 재고를 촉구하는 경우나 문제의 해결책을 제안하는 경우 등에는 주석 앞에 FIXME 나 TODO 를 붙임으로써 다른 개발자의 빠른 이해를 도울수 있다
+//  FIXME -- 해결이 필요 / TODO -- 구현이 필요
+
+
+// 18-5
+// 문제에 대한 주석으로 // FIXME: 를 사용
+class Calculator extends Abacus {
+    constructor() {
+        super();
+
+        // FIXME: 전역 변수를 사용해서는 안 됨
+        total = 0;
+    }
+}
+
+
+// 18-6
+// 문제의 해결책에 대한 주석으로 // TODO: 를 사용
+class Calculator extends Abacus {
+    constructor() {
+        super();
+
+        // TODO: total은 옵션 파라메터로 설정해야함
+        this.total = 0;
+    }
+}
